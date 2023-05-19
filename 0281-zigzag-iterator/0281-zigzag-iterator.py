97bd71d66a740775936c3024f3d491a125bbe906
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2023-05-19 00:28:52 -0400

    Time: 48 ms (10.81%), Space: 13.5 MB (97.30%) - LeetHub

--- 0281-zigzag-iterator/0281-zigzag-iterator.py
+++ 0281-zigzag-iterator/0281-zigzag-iterator.py
@@ -1,13 +1,11 @@
+import collections
 class ZigzagIterator(object):
 
     def __init__(self, v1, v2):
-        self.v = []
-        self.v.append(v1)
-        self.v.append(v2)
-        self.ptr1 = 0
-        self.ptr2 = 0
-        self.l1 = len(v1)
-        self.l2 = len(v2)
+        self.v = [v1,v2]
+        self.q = collections.deque()
+        for i,vec in enumerate(self.v):
+            if len(vec) > 0: self.q.append((i,0))
         """
         Initialize your data structure here.
         :type v1: List[int]
@@ -16,30 +14,15 @@ class ZigzagIterator(object):
 
     def next(self):
         if self.hasNext():
-            op = self.v[self.ptr1][self.ptr2]
-            if self.ptr1 == 0: self.ptr1 = 1
-            else:
-                self.ptr1 = 0
-                self.ptr2 += 1
-            return op
+            i,veci = self.q.popleft()
+            if veci + 1 < len(self.v[i]): self.q.append((i,veci+1))
+            return self.v[i][veci]
         """
         :rtype: int
         """
         
-
     def hasNext(self):
-        if self.ptr1 == 0:
-            if self.ptr2 < self.l1: return True
-            elif self.ptr2 < self.l2:
-                self.ptr1 = 1
-                return True
-        if self.ptr1 == 1:
-            if self.ptr2 < self.l2: return True
-            elif self.ptr2 < self.l1 - 1:
-                self.ptr1 = 0
-                self.ptr2 += 1
-                return True
-        return False
+        return len(self.q) > 0
         """
         :rtype: bool
         """
