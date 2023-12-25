a9bbb781c6d99aa68508152f3c9100167709278a
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2023-12-25 17:15:26 -0500

    Time: 267 ms (95.57%), Space: 19 MB (6.43%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -2,16 +2,15 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:     
         l = 1
         r = max(piles)
-        res = r
         
         while l < r:
             mid = (l + r) // 2            
             hour_spent = 0
             
             for p in piles: hour_spent += math.ceil(p / mid)
-            if hour_spent <= h: r = mid
-            else: l = mid + 1
+            if hour_spent <= h: r = mid #We keep doing this to get to the lowest val that satisfies h_s == h, r could be higher than lowest val but could satisfy the condn, so we keep coming to this condition till l == r, when the lowest possible value (l) satisfies our condn
+            else: l = mid + 1 #We need to get the lowest value that satisfies h_s == h so we will eliminate all vals from l to mid inclusive that don't satisfy here
            
-        return r
+        return l
         
         
