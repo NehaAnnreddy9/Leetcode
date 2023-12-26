be8e53f92acd28c95ae215463dd7d93ff38f7585
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2023-12-25 19:39:09 -0500

    Time: 38 ms (85.11%), Space: 18.3 MB (5.72%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,42 +11,62 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if head == None: return head
         
-        curr = head
         hm = {}
-        i = 0
-        while curr != None:
-            hm[curr] = i
-            curr = curr.next
-            i = i + 1
-            
-        hm[None] = i
-        last = i
-            
-        arr2 = []
-        curr = head
-        while curr != None:
-            arr2.append(hm[curr.random])
-            curr = curr.next
-
-        
-        arr = []
+        curr = head.next
         head1 = Node(head.val, None, None)
         st = head1
-        arr.append(st)
-        curr = head.next
+        hm[head] = head1 
         while curr != None:
             st.next = Node(curr.val, None, None)
             st = st.next
-            arr.append(st)
+            hm[curr] = st
             curr = curr.next
         
-        st = head1
-        for i in arr2:
-            if i != last: st.random = arr[i]
-            else:  st.random = None
-            st = st.next
+        hm[None] = None
+            
+        curr = head
+        while curr != None:
+            hm[curr].random = hm[curr.random]
+            curr = curr.next
             
         return head1
+        
+#         curr = head
+#         hm = {}
+#         i = 0
+#         while curr != None:
+#             hm[curr] = i
+#             curr = curr.next
+#             i = i + 1
+            
+#         hm[None] = i
+#         last = i
+            
+#         arr2 = []
+#         curr = head
+#         while curr != None:
+#             arr2.append(hm[curr.random])
+#             curr = curr.next
+
+        
+#         arr = []
+#         head1 = Node(head.val, None, None)
+#         st = head1
+#         arr.append(st)
+#         curr = head.next
+#         while curr != None:
+#             st.next = Node(curr.val, None, None)
+#             st = st.next
+#             arr.append(st)
+#             curr = curr.next
+        
+#         st = head1
+#         for i in arr2:
+#             if i != last: st.random = arr[i]
+#             else:  st.random = None
+#             st = st.next
+            
+#         return head1
             
             
         
