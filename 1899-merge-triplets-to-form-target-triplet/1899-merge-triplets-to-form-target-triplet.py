f0b4b587f48b80780ed83820ff03afb1210dc830
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2024-02-01 12:51:00 -0500

    Time: 1585 ms (90.90%), Space: 61.7 MB (72.20%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -3,14 +3,11 @@ class Solution:
         i = 0
         tva = [False, False, False]
         
-        while i < len(triplets):
-            if triplets[i][0] > target[0] or triplets[i][1] > target[1] or triplets[i][2] > target[2]: 
-                triplets.pop(i)
-            else:
+        for i in range(len(triplets)):
+            if triplets[i][0] <= target[0] and triplets[i][1] <= target[1] and triplets[i][2] <= target[2]: 
                 if triplets[i][0] == target[0]: tva[0] = True
                 if triplets[i][1] == target[1]: tva[1] = True
                 if triplets[i][2] == target[2]: tva[2] = True
-                i += 1
         
         if False in tva: return False
         return True
