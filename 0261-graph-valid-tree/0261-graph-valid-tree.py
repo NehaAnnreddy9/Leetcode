24c523c14e1960ed2979a608559504ab254fc36e
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2024-02-08 14:10:59 -0500

    Time: 90 ms (53.16%), Space: 18.6 MB (67.42%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not len(edges) and n > 1: return False
+        dt = defaultdict(list)
+        component_number = 0
+        for (u, v) in edges: 
+            dt[u].append(v)
+            dt[v].append(u)
+        visited = [False] * n
+            
+        def dfs(v, parent):
+            visited[v] = True
+            for neighbor in dt[v]:
+                if not visited[neighbor]:
+                    if not dfs(neighbor, v):
+                        return False
+                elif neighbor != parent:
+                    return False
+
+            return True
+
+        
+        for vertex in range(n):
+            if not visited[vertex]:
+                component_number += 1
+                if component_number > 1: return False  
+                if not dfs(vertex, -1): return False  
+
+        return True
+
+        
+        
+           
+                
+                
+            
+        
