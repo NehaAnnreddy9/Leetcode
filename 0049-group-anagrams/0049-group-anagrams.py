fddee3a1ed2db4b5afef18e2e4640d3819269472
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2024-04-29 21:53:33 -0400

    Time: 91 ms (52.36%), Space: 21.4 MB (29.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,44 +1,15 @@
-class Solution(object):
-    
-      def groupAnagrams(self, strs):
-        ans = {}
-        for s in strs: 
-            x = tuple(sorted(s))
-            if x in ans: ans[x].append(s)
-            else: ans[x] = [s]
-        return ans.values()
-        
-#     def groupAnagrams(self, strs): Brute force - O(m*n)
-#         ans = []
-#         hm = {}
-        
-#         for i in range(len(strs)):
-#             thm = {}
-#             for j in strs[i]:
-#                 if j in thm: thm[j] += 1
-#                 else: thm[j] = 1
-#             hm[i] = thm
-        
-#         ans.append([0])
-#         for i in range(1,len(strs)):
-#             flg = 0
-#             for j in range(0,len(ans)):
-#                 if hm[ans[j][0]] == hm[i]: 
-#                     ans[j].append(i)
-#                     flg = 1
-#             if flg == 0: ans.append([i])       
-                    
-                    
-                    
-#         for i in range(0, len(ans)):
-#             for j in range(0, len(ans[i])):
-#                 ans[i][j] = strs[ans[i][j]]
-#         return ans
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        li, hmp = [], defaultdict(list)
+        for i in range(len(strs)):
+            hmp[tuple(sorted(strs[i]))].append(i)
             
+        for i, v in hmp.items():
+            l = []
+            for j in v:
+                l.append(strs[j])
+            li.append(l)
             
-        
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+        return li
+            
         
