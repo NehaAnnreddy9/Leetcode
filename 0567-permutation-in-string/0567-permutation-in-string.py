0f06efa4c9ff68feb45fcf4259dfeb8fb3056e04
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2024-05-20 21:49:16 -0400

    Time: 55 ms (76.32%), Space: 16.7 MB (32.28%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,22 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2): return False
-        l, r = 0, len(s1) - 1
+        l, r = 0, 0
         hp1, hp2 = defaultdict(int), defaultdict(int)
         
         for i in range(len(s1)):
             hp1[s1[i]] += 1
-            hp2[s2[i]] += 1
         
         while r < len(s2):
+            hp2[s2[r]] += 1 
             if hp1 == hp2: return True
-            if hp2[s2[l]] == 1: del hp2[s2[l]]
-            else: hp2[s2[l]] -=1 
+            if (r-l+1) == len(s1):
+                if hp2[s2[l]] == 1: del hp2[s2[l]]
+                else: hp2[s2[l]] -=1 
+                l += 1
             r += 1
-            l += 1
-            if r <= len(s2) - 1: hp2[s2[r]] += 1 
-            
-        if hp1 == hp2: return True
+        
         return False
             
             
