1d816ad5352cdf929b1fdd796e705f53d55eed7d
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2024-05-31 22:32:21 -0400

    Time: 40 ms (40.16%), Space: 16.7 MB (98.67%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,27 +1,25 @@
-class Solution(object):
-    res = 0
-    def generateParenthesis(self, n):
-        self.res = []
-        self.generateParenthesisrec(n,1,0,'(')
-        return self.res
-    
-    def generateParenthesisrec(self,n,op,cl,r):
-        if op == n and cl < n:
-            while cl < n:
-                r = r + ')'
-                cl += 1
-            self.res.append(r)
-            return
-        elif op == cl and op < n:
-            r = r + "("
-            op += 1
-            self.generateParenthesisrec(n,op,cl,r)
-        elif op < n:
-            self.generateParenthesisrec(n,op+1,cl,r + '(')
-            self.generateParenthesisrec(n,op,cl+1,r + ')')
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stk = []
         
-        """
-        :type n: int
-        :rtype: List[str]
-        """
+        def func(oc, cc):
+            if oc == n and cc == n:
+                res.append("".join(stk))
+                return
+           
+            if oc < n:
+                stk.append('(')
+                func(oc + 1, cc)
+                stk.pop()
+            
+            if cc < n and cc < oc:
+                stk.append(')')
+                func(oc, cc + 1)
+                stk.pop()
+                
+        func(0, 0)
+        return res
+        
+            
         
