40bcb14ca4e9f0bb504cf42c766d80973d242a83
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2024-06-05 22:21:54 -0400

    Time: 699 ms (5.00%), Space: 73.4 MB (82.61%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,26 +1,38 @@
 class TimeMap:
 
     def __init__(self):
-        self.dt = {}
+        self.tmp = defaultdict(list)
         
+    def get_pos(self, key: str, timestamp: int) -> int:
+        l, r = 0, len(self.tmp[key])  - 1
+        while l <= r:
+            m = (l+r) // 2
+            if timestamp >= self.tmp[key][m][0]:
+                l = m + 1
+            else:
+                r = m - 1
+        return l
+        
+      
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key in self.dt:
-            self.dt[key].append((timestamp, value))
-        else: self.dt[key] = [(timestamp, value)]
-            
+        self.tmp[key].insert(self.get_pos(key, timestamp), (timestamp, value))
+           
     def get(self, key: str, timestamp: int) -> str:
+        if not len(self.tmp[key]): return ""
+        l, r = 0, len(self.tmp[key])  - 1
         res = ""
-        if key in self.dt:
-            bsv = self.dt[key]
-            l, r = 0, len(bsv) - 1
-            while l <= r:
-                m = (l + r) // 2
-                if bsv[m][0] <= timestamp:
-                    res = bsv[m][1]
-                    l = m + 1
-                else: r = m - 1
-                    
+        while l <= r:
+            m = (l+r) // 2
+            if timestamp >= self.tmp[key][m][0]:
+                res = self.tmp[key][m][1]
+                l = m + 1
+            else:
+                r = m - 1
         return res
+                
+            
+            
+       
         
 
 
